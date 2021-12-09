b1271a7d7b9b9feaa8db65d3fc49e027d9d08323
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-09 20:16:46 -0300

    Requisito 10 feito + alteração no layout

--- script.js
+++ script.js
@@ -14,12 +14,27 @@ function criarLinha(celulasTotais) {
   return linha;
 }
 
+// Muda a cor do pixel clicado
+function mudarCorPixel(event) {
+  const corSelecionada = document.querySelector('.color.selected');
+  const cor = window.getComputedStyle(corSelecionada, null).backgroundColor;
+  const pixel = event.target;
+  pixel.style.backgroundColor = cor;
+}
+
 // Coloar linhas no quadro
-function criarPixelBoard(board, linhasTotais, colunasTotais) {
+function criarPixelBoard(linhasTotais, colunasTotais) {
+  const quadroPixel = document.getElementById('pixel-board');
+  quadroPixel.innerHTML = '';
   if (linhasTotais > 0 && colunasTotais > 0) {
     for (let i = 0; i < linhasTotais; i += 1) {
-      board.appendChild(criarLinha(colunasTotais));
+      quadroPixel.appendChild(criarLinha(colunasTotais));
     }
+  }
+  pixels = document.getElementsByClassName('pixel');
+  // Adiciona opcao de mudar a cor do pixel
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', mudarCorPixel);
   }
 }
 
@@ -29,14 +44,6 @@ function selecionaCor(event) {
   event.target.classList.add('selected');
 }
 
-// Muda a cor do pixel clicado
-function mudarCorPixel(event) {
-  const corSelecionada = document.querySelector('.color.selected');
-  const cor = window.getComputedStyle(corSelecionada, null).backgroundColor;
-  const pixel = event.target;
-  pixel.style.backgroundColor = cor;
-}
-
 // Limpa o quadro
 function limparQuadro() {
   for (let i = 0; i < pixels.length; i += 1) {
@@ -44,25 +51,34 @@ function limparQuadro() {
   }
 }
 
+// Altera tamanho do quadro
+function alterarTamanhoQuadro() {
+  const boardSize = document.getElementById('board-size');
+  const tamanho = parseInt(boardSize.value, 10);
+  if (tamanho.toString() === 'NaN') {
+    window.alert('Board inválido!');
+  } else {
+    criarPixelBoard(tamanho, tamanho);
+  }
+}
+
 // Trabalhar com os elementos após a página ser carregada
 function trabalharComElementos() {
   // Pegar elementos
-  const quadroPixel = document.getElementById('pixel-board');
   const cores = document.getElementsByClassName('color');
   const botaoLimpar = document.getElementById('clear-board');
-  pixels = document.getElementsByClassName('pixel');
+  const botaoVQV = document.getElementById('generate-board');
+
   // Criar pixel board
-  criarPixelBoard(quadroPixel, 5, 5);
+  criarPixelBoard(5, 5);
   // Adiciona opcao de selecionar a cor
   for (let i = 0; i < cores.length; i += 1) {
     cores[i].addEventListener('click', selecionaCor);
   }
-  // Adiciona opcao de mudar a cor do pixel
-  for (let i = 0; i < pixels.length; i += 1) {
-    pixels[i].addEventListener('click', mudarCorPixel);
-  }
   // Adiciona opcao de limpar o quadro
   botaoLimpar.addEventListener('click', limparQuadro);
+  // Adiciona opcao de alterar o tamanho do quadro
+  botaoVQV.addEventListener('click', alterarTamanhoQuadro);
 }
 
 window.onload = trabalharComElementos;
